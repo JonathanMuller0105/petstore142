12bffff985ebda31acca018c530cb9fac1e003ae
Jonathan Muller <jonathanmuller105@gmail.com>
2025-01-21 22:20:14 -0300

    first commit

---
+++ src/test/java/TestUser.java
@@ -0,0 +1,36 @@
+// 0 - nome do pacote
+
+// 1 - Bibilioteca
+
+
+
+import org.junit.jupiter.api.Test;
+
+
+    // 2 - Classe
+public class TestUser {
+    // 2.1 atributos
+    static String ct = "application/json";
+    static String uriUser = "https://petstore.swagger.io/v2/user";
+
+    // 2.2 funções e métodos
+    // 2.2.1 funções e métodos comuns / úteis
+
+    // Função de leitura de um arquivo json
+    public static String lerarqquivoJson(String arquivoJson){
+        // TODO: Completar a  leitura do arquivo 
+        //return new String(Files);
+        return null;
+    }
+
+
+    @Test
+    public void testPostPet(){
+        // carregar os dados do arquivo json do pet
+        // TODO: Criar o metodo de POST
+        // String jsonBody = 
+
+    }
+
+    
+    }
