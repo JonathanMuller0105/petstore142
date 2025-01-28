d8de67433ab5cf7570e17bc1b00b045383e3a942
Jonathan Muller <jonathanmuller105@gmail.com>
2025-01-27 21:50:21 -0300

    Criado teste para o Post Pet

--- src/test/java/TestPet.java
+++ src/test/java/TestPet.java
@@ -2,33 +2,59 @@
 
 // 1 - Bibilioteca
 
-
+import io.restassured.response.Response; // Classe Resposta do Rest-Assured
 
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static io.restassured.RestAssured.given; // Função given
+import static org.hamcrest.CoreMatchers.*;
 
 
     // 2 - Classe
 public class TestPet {
     // 2.1 atributos
     static String ct = "application/json";
-    static String uriUser = "https://petstore.swagger.io/v2/pet";
+    static String uriPet = "https://petstore.swagger.io/v2/pet";
 
     // 2.2 funções e métodos
     // 2.2.1 funções e métodos comuns / úteis
 
     // Função de leitura de um arquivo json
-    public static String lerarqquivoJson(String arquivoJson){
-        // TODO: Completar a  leitura do arquivo 
-        //return new String(Files);
-        return null;
+    public static String lerArquivoJson(String arquivoJson) throws IOException{
+        
+        return new String(Files.readAllBytes(Paths.get(arquivoJson)));
     }
 
 
     @Test
-    public void testPostPet(){
+    public void testPostPet() throws IOException {
         // carregar os dados do arquivo json do pet
-        // TODO: Criar o metodo de POST
-        // String jsonBody = 
+        String jsonBody = lerArquivoJson("src/test/resources/Json/pet1.json");
+        int petId = 92981; // Codigo do pet
+
+        // começa o teste via rest-assured
+        // 1 - cria a instância do Rest-assured
+
+        given()
+            .contentType(ct)
+            .log().all()
+            .body(jsonBody)
+        // Execute o Post
+        .when()
+            .post(uriPet)
+            
+        // Verifique a resposta
+        .then()
+            .log().all()
+            .statusCode(200)
+            .body("name", is("Cristal"))
+            .body("id", is(petId))
+            .body("category.name", is("cachorro"))
+            .body("tags[0].name", is("vacinado"));
 
     }
 
